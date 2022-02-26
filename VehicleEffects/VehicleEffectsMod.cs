098d7172289da7d0f6eb60edc5a283032b3280ae
Acc3ssViolation <wrdbookhuis@gmail.com>
2022-02-26 17:26:03 +0100

    Rebuild for Airport update

--- VehicleEffects/VehicleEffectsMod.cs
+++ VehicleEffects/VehicleEffectsMod.cs
@@ -18,7 +18,7 @@ namespace VehicleEffects
     public partial class VehicleEffectsMod : LoadingExtensionBase, IUserMod
     {
         public const string name = "Vehicle Effects";
-        public const string version = "1.9.1";
+        public const string version = "1.9.2";
 
         private SavedBool showParseErrors;
         private SavedBool enableEditor;
